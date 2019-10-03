3a86f6e442bd0f996c112d8f7eef102251259eb9
chunche95 <37621958+chunche95@users.noreply.github.com>
2019-10-03 20:34:41 +0200

    Others

--- subida.sh
+++ subida.sh
@@ -1,22 +1,32 @@
 #!/bin/sh
 
+# Principal
 git add Exploraciondelared.html &&
 git nota "Introduccion a las redes - Networking Academy by Cisco" &&
 git sube
 sleep 2
 echo "Archivo(s) de exploracion subido"
+# Imagenes
 git add images/. &&
 git nota "Media - img" &&
 git sube
 echo "Archivo(s) de imagen(es) OK!"
 sleep 1
+# Videos
 git add videos/. &&
 git nota "Media - videos" &&
 git sube
 echo "Archivo(s) de video(s) OK!"
+# Documentos
 git add documentos/. &&
 git nota "Media - Docs" &&
 git sube
 echo "Archivo(s) docs OK!"
+sleep 2
+# Other - Otros.
+git add . &&
+git nota "Others" && 
+git sube
+echo "Otros archivos  subidos"
 sleep 2
 # clear
